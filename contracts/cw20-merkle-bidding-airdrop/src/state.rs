8ec18c9ea7d813f55ff274133c90ea7b9671be69
stepyt <stepyt.crypto@gmail.com>
2022-06-01 16:24:57 +0200

    Modified state with new stage structure.

--- contracts/cw20-merkle-bidding-airdrop/src/state.rs
+++ contracts/cw20-merkle-bidding-airdrop/src/state.rs
@@ -1,53 +1,57 @@
-use std::ops::Add;
-
 use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
+use serde::{ Deserialize, Serialize };
+use cosmwasm_std::{ Addr, Uint128 };
+use cw_storage_plus::{ Item, Map };
+use cw_utils::{ Scheduled, Duration };
 
-use cosmwasm_std::{Addr, Uint128};
-use cw_storage_plus::{Item, Map};
-use cw_utils::{Expiration, Scheduled};
-
+/// Struct to manage the contract configuration.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
     /// Owner If None set, contract is frozen.
     pub owner: Option<Addr>,
     pub cw20_token_address: Addr,
-    //ciao ste
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-/// struct to manage start and end of static stages
+/// Struct to manage start and end of static stages.
 pub struct Stage {
-    /// start variable will be scheduled by time or block
+    /// Starting event for the stage.
     pub start: Scheduled,
-    /// end variable will be scheduled by time or block
-    /// or, if needed, never.
-    pub end: Expiration,
+    /// Ending event for the stage.
+    pub duration: Duration,
 }
 
+/// Storage to manage contract configuration.
 pub const CONFIG_KEY: &str = "config";
 pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
 
+/// Storage for the bid stage info.
 pub const STAGE_BID_KEY: &str = "stage_bid";
 pub const STAGE_BID: Item<Stage> = Item::new(STAGE_BID_KEY);
 
+/// Storage for the airdrop stage info.
 pub const STAGE_CLAIM_AIRDROP_KEY: &str = "stage_claim_airdrop";
 pub const STAGE_CLAIM_AIRDROP: Item<Stage> = Item::new(STAGE_CLAIM_AIRDROP_KEY);
 
+/// Storage for the claiming prize stage info.
 pub const STAGE_CLAIM_PRIZE_KEY: &str = "stage_claim_prize";
 pub const STAGE_CLAIM_PRIZE: Item<Stage> = Item::new(STAGE_CLAIM_PRIZE_KEY);
 
-pub const WINNING_ADDRESSES_KEY: &str = "winning_addresses";
-pub const WINNING_ADDRESSES: Map<&Addr, bool> = Map::new(WINNING_ADDRESSES_KEY);
-
+/// Storage to save the first game ticket prize.
 pub const TICKET_PRICE_KEY: &str = "ticket_price";
 pub const TICKET_PRICE: Item<Uint128> = Item::new(TICKET_PRICE_KEY);
 
 pub const MERKLE_ROOT_PREFIX: &str = "merkle_root";
-pub const MERKLE_ROOT: Item<String> = Item::new(MERKLE_ROOT_PREFIX);
+pub const MERKLE_ROOT: Map<u8, String> = Map::new(MERKLE_ROOT_PREFIX);
 
-pub const CLAIM_PREFIX: &str = "claim";
-pub const CLAIM: Map<&Addr, bool> = Map::new(CLAIM_PREFIX);
+/// Storage to save if an addres has claimed the airdrop or not
+pub const CLAIM_AIRDROP_PREFIX: &str = "claim_airdrop";
+pub const CLAIM_AIRDROP: Map<&Addr, bool> = Map::new(CLAIM_AIRDROP_PREFIX);
 
+/// Storage to save if a winning addres has claimed the prize or not
+pub const CLAIM_PRIZE_PREFIX: &str = "claim_prize";
+pub const CLAIM_PRIZE: Map<&Addr, bool> = Map::new(CLAIM_PRIZE_PREFIX);
+
+/// Storage to manage the bid of each addres.
 pub const BIDS_PREFIX: &str = "bids";
 pub const BIDS: Map<&Addr, Uint128> = Map::new("bids");
